0819bc790abebcaf0033f747edb7728e775210e7
schemabuoi <charyszczakg@gmail.com>
2019-05-01 11:25:46 +0200

    Update MainViewController.java
    Implementing IMainModel injection

--- BelTracker/src/beltracker/gui/controller/MainViewController.java
+++ BelTracker/src/beltracker/gui/controller/MainViewController.java
@@ -5,22 +5,47 @@
  */
 package beltracker.gui.controller;
 
+import beltracker.exception.BeltrackerException;
+import beltracker.gui.model.IMainModel;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.layout.TilePane;
 
 /**
+ * FXML Controller class
  *
  * @author Acer
  */
 public class MainViewController implements Initializable {
+
+    private IMainModel model;
     
+    @FXML
+    private TilePane tilOrders;
+    
+    /**
+     * Initializes the controller class.
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+    
+    public void injectModel(IMainModel model)
+    {
+        this.model = model;
+    }
+    
+    public void loadOrders() throws BeltrackerException
+    {
+        model.loadOrders();
+//        List<Order> orders = model.getOrders();
+//        for (Order order : orders) 
+//        {
+//            
+//        }      
+    }
     
 }
