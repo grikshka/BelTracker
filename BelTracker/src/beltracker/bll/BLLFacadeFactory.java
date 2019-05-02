d4aaa34b43698cc54f18a6b5cff49ea474f6d052
schemabuoi <charyszczakg@gmail.com>
2019-05-02 12:08:22 +0200

    Create BLLFacadeFactory.java

---
+++ BelTracker/src/beltracker/bll/BLLFacadeFactory.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beltracker.bll;
+
+/**
+ *
+ * @author Acer
+ */
+public class BLLFacadeFactory {
+    
+    public enum FacadeType {
+        PRODUCTION
+    }
+    
+    
+    private static BLLFacadeFactory instance;
+    
+    private BLLFacadeFactory()
+    {
+        
+    }
+    
+    public static synchronized BLLFacadeFactory getInstance()
+    {
+        if(instance == null)
+        {
+            return new BLLFacadeFactory();
+        }
+        return instance;
+    }
+    
+    public IBLLFacade createFacade(FacadeType type)
+    {
+        switch(type)
+        {
+            case PRODUCTION: return new BLLManager();
+            
+            default:         return new BLLManager();
+            
+        }
+    }
+
+    
+}
