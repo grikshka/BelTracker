dc331a5d2c4a6cfab83dfa36699c653cd0396c9b
schemabuoi <charyszczakg@gmail.com>
2019-05-02 12:08:06 +0200

    Create DALManager.java

---
+++ BelTracker/src/beltracker/dal/DALManager.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beltracker.dal;
+
+import beltracker.be.Order;
+import java.util.List;
+
+/**
+ *
+ * @author Acer
+ */
+public class DALManager implements IDALFacade{
+
+    @Override
+    public List<Order> getOrders(String departmentName) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
+}
