66d7a2b655648e4bccb61999bd8c84b2614c6952
schemabuoi <charyszczakg@gmail.com>
2019-05-03 11:48:29 +0200

    Update Order.java
    Adding deliveryDate property

--- BelTracker/src/beltracker/be/Order.java
+++ BelTracker/src/beltracker/be/Order.java
@@ -5,8 +5,11 @@
  */
 package beltracker.be;
 
+import java.time.LocalDate;
 import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
@@ -21,14 +24,16 @@ public class Order {
     private final StringProperty departmentName = new SimpleStringProperty();
     private final DoubleProperty realizedProgress = new SimpleDoubleProperty();
     private final DoubleProperty estimatedProgress = new SimpleDoubleProperty();
+    private final ObjectProperty<LocalDate> deliveryDate = new SimpleObjectProperty<>();
     
-    public Order(String orderNumber, String customerName, String departmentName, double realizedProgress, double estimatedProgress)
+    public Order(String orderNumber, String customerName, String departmentName, double realizedProgress, double estimatedProgress, LocalDate deliveryDate)
     {
         this.orderNumber.set(orderNumber);
         this.customerName.set(customerName);
         this.departmentName.set(departmentName);
         this.realizedProgress.set(realizedProgress);
         this.estimatedProgress.set(estimatedProgress);
+        this.deliveryDate.set(deliveryDate);
     }
 
     public String getOrderNumber() {
@@ -89,6 +94,18 @@ public class Order {
 
     public DoubleProperty estimatedProgressProperty() {
         return estimatedProgress;
+    }
+    
+    public LocalDate getDeliveryDate() {
+        return deliveryDate.get();
+    }
+
+    public void setDeliveryDate(LocalDate value) {
+        deliveryDate.set(value);
+    }
+
+    public ObjectProperty deliveryDateProperty() {
+        return deliveryDate;
     }
     
 }
