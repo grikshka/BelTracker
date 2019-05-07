2686d786f37e4c2526ef3c45e302799ce43f1817
schemabuoi <charyszczakg@gmail.com>
2019-05-07 09:37:07 +0200

    Update BLLManager.java
    Implement checkOrderStatus and calculateEstimatedProgress methods

--- BelTracker/src/beltracker/bll/BLLManager.java
+++ BelTracker/src/beltracker/bll/BLLManager.java
@@ -6,8 +6,11 @@
 package beltracker.bll;
 
 import beltracker.be.Order;
+import beltracker.be.Order.OrderStatus;
 import beltracker.dal.DALFacadeFactory;
 import beltracker.dal.IDALFacade;
+import java.time.LocalDate;
+import static java.time.temporal.ChronoUnit.DAYS;
 import java.util.List;
 
 /**
@@ -25,9 +28,52 @@ public class BLLManager implements IBLLFacade{
 
     @Override
     public List<Order> getOrders(String departmentName) {
-        return facade.getOrders(departmentName);
+        List<Order> orders = facade.getOrders(departmentName);
+        for(Order order : orders)
+        {
+            OrderStatus status = checkOrderStatus(order, departmentName);
+            double estimatedProgress = calculateEstimatedProgress(order);
+            order.setOrderStatus(status);
+            order.setEstimatedProgress(estimatedProgress);
+        }
+        return orders;
     }
     
+    private OrderStatus checkOrderStatus(Order order, String departmentName)
+    {
+        if(LocalDate.now().isAfter(order.getDeliveryDate()))
+        {
+            return OrderStatus.DELAYED;
+        }
+        else if(!order.getDepartmentName().equals(departmentName))
+        {
+            return OrderStatus.OVERDUE;
+        }
+        else
+        {
+            return OrderStatus.ON_SCHEDULE;
+        }
+    }
     
+    private double calculateEstimatedProgress(Order order)
+    {
+        LocalDate currentDate = LocalDate.now();
+        LocalDate startDate = order.getStartDate();
+        LocalDate deliveryDate = order.getDeliveryDate();
+        if(currentDate.isBefore(startDate))
+        {
+            return 0;
+        }
+        else if(currentDate.isBefore(deliveryDate))
+        {
+            double totalNumberOfDays = DAYS.between(startDate, deliveryDate);
+            double currentNumberOfDays = DAYS.between(startDate, currentDate);
+            return currentNumberOfDays/totalNumberOfDays;
+        }
+        else
+        {
+            return 1;
+        }
+    }
     
 }
