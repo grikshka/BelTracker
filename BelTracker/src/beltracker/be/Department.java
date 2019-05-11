c21e5c673f9820ae53146a4fc95763b0655743f1
schemabuoi <charyszczakg@gmail.com>
2019-05-11 11:53:41 +0200

    Create Department.java
    Create business entity for department

---
+++ BelTracker/src/beltracker/be/Department.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beltracker.be;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+/**
+ *
+ * @author Acer
+ */
+public class Department {
+
+    private final IntegerProperty id = new SimpleIntegerProperty();
+    private final StringProperty name = new SimpleStringProperty();
+
+    public Department(int id, String name)
+    {
+        this.id.set(id);
+        this.name.set(name);
+    }
+    
+    public String getName() {
+        return name.get();
+    }
+
+    public void setName(String value) {
+        name.set(value);
+    }
+
+    public StringProperty nameProperty() {
+        return name;
+    }
+
+    public int getId() {
+        return id.get();
+    }
+
+    public void setId(int value) {
+        id.set(value);
+    }
+
+    public IntegerProperty idProperty() {
+        return id;
+    }
+    
+    @Override
+    public String toString()
+    {
+        return name.get();
+    }
+    
+}
