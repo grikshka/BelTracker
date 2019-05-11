bcc7fcc1b256670bf22c35b8bfd73049fb7f1def
schemabuoi <charyszczakg@gmail.com>
2019-05-11 12:09:37 +0200

    Update MockDALManager.java
    Add mocking departments

--- BelTracker/src/beltracker/dal/MockDALManager.java
+++ BelTracker/src/beltracker/dal/MockDALManager.java
@@ -5,6 +5,7 @@
  */
 package beltracker.dal;
 
+import beltracker.be.Department;
 import beltracker.be.Order;
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -16,15 +17,18 @@ import java.util.Random;
  * @author Acer
  */
 public class MockDALManager implements IDALFacade{
+    
     private final int AMOUNT_OF_ORDERS = 100;
     private final double ORDER_OVERDUE_PROBABILITY = 0.10;
     private final double ORDER_DELAYED_PROBABILITY = 0.10;
+    private final List<Department> departments = new ArrayList<>();
     private final Random randGenerator = new Random();
     private final List<String> customers = new ArrayList<>();
     
     public MockDALManager()
     {
         initializeCustomersList();
+        initializeDepartmentsList();
     }
     
     private void initializeCustomersList()
@@ -40,64 +44,82 @@ public class MockDALManager implements IDALFacade{
         customers.add("ChemChina");
         customers.add("Norinco");
     }
+    
+    
+    private void initializeDepartmentsList() 
+    {
+        departments.add(new Department(1, "Halvfab"));
+        departments.add(new Department(2, "Bælg"));
+        departments.add(new Department(3, "Montage 1"));
+        departments.add(new Department(4, "Montage 2"));
+        departments.add(new Department(5, "Bertel"));
+        departments.add(new Department(6, "Maler"));
+        departments.add(new Department(7, "Forsendelse"));       
+    }
 
     @Override
-    public List<Order> getOrders(String departmentName) 
+    public List<Order> getOrders(Department department) 
     {
         List<Order> departmentOrders = new ArrayList<>();
         for(int i = 0; i < AMOUNT_OF_ORDERS; i++)
         {
-            Order order = generateOrder(departmentName);
+            Order order = generateOrder(department);
             departmentOrders.add(order);
         }
         return departmentOrders;
     }
     
-    private Order generateOrder(String departmentName)
+    @Override
+    public List<Department> getDepartments() {
+        return departments;
+    }
+    
+    private Order generateOrder(Department department)
     {
         double randomDouble = randGenerator.nextDouble();
         if(randomDouble < ORDER_DELAYED_PROBABILITY)
         {
-            return generateDelayedOrder(departmentName);
+            return generateDelayedOrder(department);
         }
         else if(randomDouble < ORDER_DELAYED_PROBABILITY + ORDER_OVERDUE_PROBABILITY)
         {
-            return generateOverdueOrder();
+            return generateOverdueOrder(department);
         }
         else
         {
-            return generateOnScheduleOrder(departmentName);
+            return generateOnScheduleOrder(department);
         }
     }
     
-    private Order generateDelayedOrder(String departmentName)
+    private Order generateDelayedOrder(Department department)
     {
         String orderNumber = generateOrderNumber();
         String customerName = customers.get(randGenerator.nextInt(customers.size()));
         LocalDate currentDate = LocalDate.now();
         LocalDate startDate = currentDate.minusDays(randGenerator.nextInt(7) + 4);
         LocalDate deliveryDate = currentDate.minusDays(randGenerator.nextInt(2) + 1);
-        return new Order(orderNumber, customerName, departmentName, startDate, deliveryDate);
+        return new Order(department, orderNumber, customerName, startDate, deliveryDate);
     }
     
-    private Order generateOverdueOrder()
+    private Order generateOverdueOrder(Department department)
     {
         String orderNumber = generateOrderNumber();
         String customerName = customers.get(randGenerator.nextInt(customers.size()));
         LocalDate currentDate = LocalDate.now();
         LocalDate startDate = currentDate.minusDays(randGenerator.nextInt(2) + 1);
         LocalDate deliveryDate = currentDate.plusDays(randGenerator.nextInt(7) + 1);
-        return new Order(orderNumber, customerName, "Bertel", startDate, deliveryDate);
+        Department overdueDepartment = generateOverdueDepartment(department);
+        return new Order(overdueDepartment, orderNumber, customerName, startDate, deliveryDate);
     }
     
-    private Order generateOnScheduleOrder(String departmentName)
+    private Order generateOnScheduleOrder(Department department)
     {
         String orderNumber = generateOrderNumber();
         String customerName = customers.get(randGenerator.nextInt(customers.size()));
         LocalDate currentDate = LocalDate.now();
         LocalDate startDate = currentDate.minusDays(randGenerator.nextInt(4) + 1);
         LocalDate deliveryDate = currentDate.plusDays(randGenerator.nextInt(4) + 1);
-        return new Order(orderNumber, customerName, departmentName, startDate, deliveryDate);
+        return new Order(department, orderNumber, customerName, startDate, deliveryDate);
     }
     
     private String generateOrderNumber()
@@ -110,4 +132,16 @@ public class MockDALManager implements IDALFacade{
         orderNumber += randGenerator.nextInt(90) + 10;
         return orderNumber;
     }
+    
+    private Department generateOverdueDepartment(Department department)
+    {
+        Department overdueDepartment;
+        do
+        {
+            overdueDepartment = departments.get(randGenerator.nextInt(departments.size()));
+        }
+        while(overdueDepartment.getId() == department.getId());
+        return overdueDepartment;
+    }
+    
 }
