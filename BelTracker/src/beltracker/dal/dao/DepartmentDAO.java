606323ed3e2b3d9ba33a7c4512f788c170125968
schemabuoi <charyszczakg@gmail.com>
2019-05-13 10:42:02 +0200

    Create DepartmentDAO.java

---
+++ BelTracker/src/beltracker/dal/dao/DepartmentDAO.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beltracker.dal.dao;
+
+import beltracker.be.Department;
+import java.sql.Connection;
+import java.util.List;
+
+/**
+ *
+ * @author Acer
+ */
+public class DepartmentDAO {
+    
+    public List<Department> getAllDepartments(Connection con)
+    {
+        //TO DO 
+        return null;
+    }
+    
+}
