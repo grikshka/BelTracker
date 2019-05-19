bbc32f97e5623ec8727aa86ed9fcab124a71cdb0
schemabuoi <charyszczakg@gmail.com>
2019-05-19 21:04:37 +0200

    Implementing TaskFullView and TaskFullViewController

---
+++ BelTracker/src/beltracker/gui/controller/TaskFullViewController.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beltracker.gui.controller;
+
+import beltracker.be.Task;
+import beltracker.gui.model.interfaces.ITaskModel;
+import beltracker.gui.util.AlertManager;
+import beltracker.gui.util.AnimationPlayer;
+import com.jfoenix.controls.JFXProgressBar;
+import java.net.URL;
+import java.time.format.DateTimeFormatter;
+import java.util.ResourceBundle;
+import javafx.application.Platform;
+import javafx.beans.binding.Bindings;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ToggleGroup;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author Kiddo
+ */
+public class TaskFullViewController implements Initializable {
+
+    private static final String DATE_FORMAT = "dd MMMM yyyy";
+    
+    private ITaskModel model;
+    private AnimationPlayer animationPlayer;
+    
+    @FXML
+    private Button btnStart;
+    @FXML
+    private Label lblStartDate;
+    @FXML
+    private Label lblEndDate;
+    @FXML
+    private Label lblOrderNumber;
+    @FXML
+    private Label lblOrderCurrentDepartment;
+    @FXML
+    private JFXProgressBar prgEstimatedProgress;
+    @FXML
+    private Label lblOrderCustomerName;
+
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        Platform.runLater(() -> showStageWithAnimation());
+    }    
+    
+    public void injectModel(ITaskModel model)
+    {
+        this.model = model;
+        setTaskDetails(model.getTask());
+    }
+    
+    private void setTaskDetails(Task task)
+    {
+        prgEstimatedProgress.progressProperty().bind(task.estimatedProgressProperty());
+        
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern(DATE_FORMAT);
+        lblStartDate.textProperty().bind(Bindings.createStringBinding(() ->
+               dtf.format(task.getStartDate()), task.startDateProperty()));
+        lblEndDate.textProperty().bind(Bindings.createStringBinding(() ->
+               dtf.format(task.getEndDate()), task.endDateProperty()));
+        lblOrderCustomerName.textProperty().bind(Bindings.createStringBinding(() ->
+                task.getOrder().getCustomerName(), task.orderProperty()));
+        lblOrderNumber.textProperty().bind(Bindings.createStringBinding(() ->
+                task.getOrder().getNumber(), task.orderProperty()));
+        lblOrderCurrentDepartment.textProperty().bind(Bindings.createStringBinding(() ->
+                task.getOrder().getCurrentDepartment().getName(), task.orderProperty()));
+    }
+    
+    public void showStageWithAnimation()
+    {
+        Stage stage = (Stage) btnStart.getScene().getWindow();
+        animationPlayer = new AnimationPlayer();
+        animationPlayer.playSlideAndShow(stage);
+    }    
+    
+
+    @FXML
+    private void clickClose(ActionEvent event)
+    {
+        Stage stage = (Stage) btnStart.getScene().getWindow();
+        animationPlayer = new AnimationPlayer();
+        animationPlayer.playSlideAndClose(stage);
+    }
+
+    @FXML
+    private void clickSubmit(ActionEvent event)
+    {
+        AlertManager alertManager = new AlertManager();
+//        alertManager.displayConfirmation("Submit task","Are you sure you want to submit this task as finished?");
+    }
+    
+}
