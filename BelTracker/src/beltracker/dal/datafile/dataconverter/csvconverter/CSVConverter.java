12729cb4cbd56f927a3bbe4a172389682ed87c12
schemabuoi <charyszczakg@gmail.com>
2019-06-01 13:50:07 +0200

    Create bodies for XLSX and CSV converter

---
+++ BelTracker/src/beltracker/dal/datafile/dataconverter/csvconverter/CSVConverter.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beltracker.dal.datafile.dataconverter.csvconverter;
+
+import beltracker.dal.datatransfer.DataTransfer;
+
+/**
+ *
+ * @author Acer
+ */
+public class CSVConverter {
+    
+    public DataTransfer convertFileData(String filePath)
+    {
+        //TO DO
+        return null;
+    }
+    
+}
